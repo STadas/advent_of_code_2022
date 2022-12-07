fa887161385f63a89b094eb94b808b51797bc873
STadas <saltenis.tadas@gmail.com>
2022-12-07 22:22:45 +0200

    day 7 kinda cleanup

--- 07/solution.lisp
+++ 07/solution.lisp
@@ -1,4 +1,3 @@
-(defvar *size-sum* 0)
 (defvar *dir-stack* (list))
 (defvar *dir-map* (make-hash-table :test 'equal))
 (defun handle-command (command output)
@@ -12,29 +11,31 @@
     (setf (gethash (format nil "~{~a/~}" (reverse *dir-stack*))
                    *dir-map*) output)))
 
-(defun dir-size (dir)
-  (sum (loop for item in (gethash dir *dir-map*)
-             collect
-             (if (str:starts-with-p "dir" item)
-               (let* ((dirname (concatenate 'string dir (nth 1 (str:split " " item)) "/"))
-                      (dsize (dir-size dirname)))
-                 (if (<= dsize 100000) (incf *size-sum* dsize))
-                 dsize)
-               (parse-integer (nth 0 (str:split " " item)))))))
-
-(defun p1 (data)
+(defun gen-dir-map (data)
   (loop for line in data
         for i from 1
         when (equal (nth 0 (str:split " " line)) "$")
         collect
         (handle-command (subseq (str:split " " line) 1)
-                        (subseq data i (position-if
-                                         (lambda (s) (str:starts-with-p "$" s))
-                                         data :start i))))
-  (dir-size "//")
-  *size-sum*)
+                        (subseq data i
+                                (position-if
+                                  (lambda (s) (str:starts-with-p "$" s))
+                                  data :start i)))))
 
-(defun p2 (data)
+(defun dir-size (dir)
+  (sum (loop for item in (gethash dir *dir-map*)
+             collect
+             (if (str:starts-with-p "dir" item)
+               (dir-size (concatenate 'string dir (nth 1 (str:split " " item)) "/"))
+               (parse-integer (nth 0 (str:split " " item)))))))
+
+(defun p1 ()
+  (sum (remove-if
+         (lambda (size)(> size 100000))
+         (loop for key being the hash-keys of *dir-map*
+               collect (dir-size key)))))
+
+(defun p2 ()
   (min-in-list
     (remove-if
       (lambda (size)
@@ -42,7 +43,7 @@
       (loop for key being the hash-keys of *dir-map*
             collect (dir-size key)))))
 
-
 (defun day-main ()
-  (format t "p1=~a~%" (p1 (input-lines *aoc-day*)))
-  (format t "p2=~a~%" (p2 (input-lines *aoc-day*))))
+  (gen-dir-map (input-lines *aoc-day*))
+  (format t "p1=~a~%" (p1))
+  (format t "p2=~a~%" (p2)))
